6a0eb9d79ffbbddfa73fab64579d6fd2fe24cf9f
Seta <sweater@azusasoft.com>
2017-06-12 13:33:11 +0800

    first

---
+++ swipeBackUtility/src/main/java/com/seta/swipebackutility/app/SwipeBackActivityBase.java
@@ -0,0 +1,21 @@
+package com.seta.swipebackutility.app;
+
+import com.seta.swipebackutility.SwipeBackLayout;
+
+/**
+ * @author Yrom
+ */
+public interface SwipeBackActivityBase {
+    /**
+     * @return the SwipeBackLayout associated with this activity.
+     */
+    public abstract SwipeBackLayout getSwipeBackLayout();
+
+    public abstract void setSwipeBackEnable(boolean enable);
+
+    /**
+     * Scroll out contentView and finish the activity
+     */
+    public abstract void scrollToFinishActivity();
+
+}
